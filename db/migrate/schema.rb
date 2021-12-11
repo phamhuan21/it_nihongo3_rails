a4c96dd0eaae61402fa6ad7298789020cb863424
phamhuan21 <thiensuquanquan@gmail.com>
2021-12-12 01:36:17 +0700

    Add review book

--- db/migrate/schema.rb
+++ db/migrate/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_27_015000) do
+ActiveRecord::Schema.define(version: 2020_11_27_020659) do
 
     # These are extensions that must be enabled in order to support this database
     enable_extension "plpgsql"
@@ -33,6 +33,13 @@ ActiveRecord::Schema.define(version: 2020_11_27_015000) do
         t.string "checksum", null: false
         t.datetime "created_at", null: false
         t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+      end
+      create_table "book_reviews", force: :cascade do |t|
+        t.integer "user_id"
+        t.integer "book_id"
+        t.text "review"
+        t.datetime "created_at", precision: 6, null: false
+        t.datetime "updated_at", precision: 6, null: false
       end
     create_table "books", force: :cascade do |t|
       t.string "title"
