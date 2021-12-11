8216c44e320d20ffb9fa3c230e7c4adaeb04350f
phamhuan21 <thiensuquanquan@gmail.com>
2021-12-11 23:23:10 +0700

    Add detail book

--- db/migrate/schema.rb
+++ db/migrate/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_05_084635) do
+ActiveRecord::Schema.define(version: 2020_11_27_015000) do
 
     # These are extensions that must be enabled in order to support this database
     enable_extension "plpgsql"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 2021_12_05_084635) do
     create_table "books", force: :cascade do |t|
       t.string "title"
       t.integer "number_of_pages"
+      t.text "detail"
       t.datetime "created_at", null: false
       t.datetime "updated_at", null: false
     end
